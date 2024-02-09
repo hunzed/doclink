d41f8d3915d407b47bc2940331050e16136b1797
hunzed <101815330+hunzed@users.noreply.github.com>
2024-02-09 20:48:43 +0300

    main page done

---
+++ docklink-app/app/page.js
@@ -0,0 +1,31 @@
+import React from "react"
+import Link from "next/link"
+
+export default function Component() {
+  return (
+    <div className="mx-auto max-w-5xl flex flex-col items-center justify-center p-6 space-y-6">
+      <header className="absolute top-0 right-0 p-4">
+        <Link className="mr-4" href="#">
+        <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Doctor Portol</button>
+        </Link>
+        <Link href="#">
+          <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">SMS subscription</button>
+
+        </Link>
+      </header>
+      <div className="text-center space-y-2">
+        <p className="text-sm font-semibold tracking-wide text-cyan-600 dark:text-cyan-400">DOCLINK</p>
+        <h1 className="text-3xl font-bold">We are here to help</h1>
+      </div>
+      <div className="text-center space-y-2">
+        <p className="text-sm text-gray-500 dark:text-gray-400">Do not reveal any personal information!</p>
+      </div>
+      <div className="flex flex-col gap-2">
+        <Link className="w-full" href="#">
+          <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Connect to Doctors</button>
+
+        </Link>
+      </div>
+    </div>
+  )
+}
